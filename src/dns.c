9d8b840fe4e18340cba4df615919eca9559ac8f6
klewy <cigankov807@gmail.com>
2025-11-08 20:23:07 +0300

    Cleanup

--- src/dns.c
+++ src/dns.c
@@ -10,23 +10,25 @@
 #include <stdbool.h>
 
 int send_dns_request(int sock, int qtype, const char* domain) {
-    int ret = 0;
+    int ret;
+    uint16_t flags;
     char buf[MAX_DNS_LENGTH];
-    char* buf_ptr = buf;
-    struct dns_header* dns_hdr = (struct dns_header*)buf_ptr;
+    char* ptr = buf;
+    struct dns_header* dns_hdr;
+    struct dns_question* dns_q;
+
+    dns_hdr = (struct dns_header*)ptr;
     memset(dns_hdr, 0, sizeof(*dns_hdr));
     dns_hdr->id = htons(1337);
-
-    uint16_t flags = 0;
+    flags = 0;
     flags |= RD_BIT;
     flags &= ~QR_FLAG;
     dns_hdr->flags = htons(flags);
-
     dns_hdr->qdcount = htons(1);
     dns_hdr->ancount = htons(0);
     dns_hdr->nscount = htons(0);
     dns_hdr->arcount = htons(0);
-    buf_ptr += sizeof(*dns_hdr);
+    ptr += sizeof(*dns_hdr);
 
     char* qname_bytes = process_domain(domain);
     if (!qname_bytes) {
@@ -34,43 +36,44 @@ int send_dns_request(int sock, int qtype, const char* domain) {
         return -1;
     }
     size_t qname_len = strlen(qname_bytes) + 1;
-    memcpy(buf_ptr, qname_bytes, qname_len);
-    buf_ptr += qname_len;
+    memcpy(ptr, qname_bytes, qname_len);
+    free(qname_bytes);
+    ptr += qname_len;
 
-    struct dns_question* dns_q = (struct dns_question*)(buf_ptr);
+    dns_q = (struct dns_question*)(ptr);
     memset(dns_q, 0, sizeof(*dns_q));
-
     dns_q->qtype = htons(qtype);
     dns_q->qclass = htons(0x0001);
-    buf_ptr += sizeof(*dns_q);
+    ptr += sizeof(*dns_q);
 
-    ret = send(sock, buf, buf_ptr - buf, 0);
+    ret = send(sock, buf, ptr - buf, 0);
     if (ret < 0) {
         perror("send failed");
         return -1;
     }
 
-    free(qname_bytes);
     return 0;
 }
 
 int parse_dns_response(int sock) {
-
+    ssize_t rd_bytes;
     char buf[MAX_DNS_LENGTH];
-    char* buf_ptr = buf;
-    ssize_t rd_bytes = read(sock, buf, sizeof(buf));
+    char* ptr = buf;
+    uint16_t flags;
+    struct dns_header* dns_hdr;
+    struct dns_question* dns_q;
+
+    rd_bytes = read(sock, buf, sizeof(buf));
     if (rd_bytes < 0) {
         perror("read error");
         return -1;
     }
 
 
-    struct dns_header* dns_hdr = (struct dns_header*)buf_ptr;
-    buf_ptr += sizeof(*dns_hdr);
-
-    uint16_t flags = ntohs(dns_hdr->flags);
+    dns_hdr = (struct dns_header*)ptr;
+    flags = ntohs(dns_hdr->flags);
     bool is_recursion_available = (flags & RA_BIT) == RA_BIT;
-    bool is_aa = (flags & AA_BIT) == AA_BIT;
+    // bool is_aa = (flags & AA_BIT) == AA_BIT;
     int rcode = (flags & RCODE_MASK);
     if (!is_recursion_available) {
         fprintf(stderr, "Recursion is not available, failing..\n");
@@ -80,48 +83,39 @@ int parse_dns_response(int sock) {
         fprintf(stderr, "Server error: %d, failing..\n", rcode);
         return -1;
     }
-    // if (!is_aa) {
-    //     fprintf(stderr, "Not authoritive answers are not supported\n");
-    //     return -1;
-    // }
 
-    char* qname = buf_ptr;
+    ptr += sizeof(struct dns_header);
+
+    char* qname = ptr;
     if ((*qname & 0xC0) == 0xC0) {
         fprintf(stderr, "DNS Compression is not supported yet\n");
         return -1;
     }
     size_t qname_len = strlen(qname) + 1;
-    buf_ptr += qname_len;
+    ptr += qname_len;
 
-    struct dns_question* dns_q = (struct dns_question*)buf_ptr;
-    buf_ptr += sizeof(*dns_q);
-    uint16_t qtype = ntohs(dns_q->qtype);
-    uint16_t qclass = ntohs(dns_q->qclass);
-
+    dns_q = (struct dns_question*)ptr;
+    ptr += sizeof(struct dns_question);
 
     int i = 0;
     while (i < ntohs(dns_hdr->ancount)) {
-        char* name = buf_ptr;
+        char* name = ptr;
         if ((*name & 0xC0) == 0xC0) {
             fprintf(stderr, "DNS Compression is not supported yet\n");
             return -1;
         }
         size_t name_len = strlen(name) + 1;
-        buf_ptr += name_len;
+        ptr += name_len;
 
-        struct dns_answer* dns_a = (struct dns_answer*)buf_ptr;
-        buf_ptr += sizeof(*dns_a);
+        struct dns_answer* dns_a = (struct dns_answer*)ptr;
+        ptr += sizeof(*dns_a);
 
 
         uint16_t type = ntohs(dns_a->type);
-        uint16_t class_ = ntohs(dns_a->class_);
-        uint32_t ttl = ntohl(dns_a->ttl);
-        uint16_t rdlength = ntohs(dns_a->rdlength);
-
         switch (type) {
             case 1: { // IPv4
-                uint32_t ip = *(uint32_t*)buf_ptr;
-                buf_ptr += sizeof(uint32_t);
+                uint32_t ip = *(uint32_t*)ptr;
+                ptr += sizeof(uint32_t);
 
                 char ip_str[INET_ADDRSTRLEN];
                 const char* r = inet_ntop(AF_INET, &ip, ip_str, sizeof(ip_str));
@@ -133,8 +127,8 @@ int parse_dns_response(int sock) {
                 break;
             }
             case 28: { // IPv6
-                char* ipv6_buf = buf_ptr; // 16 bytes
-                buf_ptr += 16; // ipv6 is 16 bytes (int128)
+                char* ipv6_buf = ptr; // 16 bytes
+                ptr += 16; // ipv6 is 16 bytes (int128)
 
                 char ip_str[INET6_ADDRSTRLEN];
                 const char* r = inet_ntop(AF_INET6, &ipv6_buf, ip_str, sizeof(ip_str));
@@ -146,8 +140,6 @@ int parse_dns_response(int sock) {
                 break;
             }
         }
-
-
         ++i;
     }
     return 0;
--- src/main.c
+++ src/main.c
@@ -64,56 +64,6 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-
-    // close(sock);
+    close(sock);
     return 0;
 }
-
-
-
-
-// struct addrinfo hints;
-// memset(&hints, 0, sizeof(hints));
-// hints.ai_family = AF_INET;
-// hints.ai_socktype = SOCK_STREAM;
-// hints.ai_protocol = 0;
-
-// struct addrinfo* result;
-// int ret = getaddrinfo("google.com", "443", &hints, &result);
-// if (ret < 0) {
-//     perror("Get addr info failed");
-//     return -1;
-// }
-
-// struct addrinfo* ptr = NULL;
-// for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
-//     switch (ptr->ai_family) {
-//         case AF_INET: {
-//             struct sockaddr_in* addr = (struct sockaddr_in*)ptr->ai_addr;
-//             char buf[INET_ADDRSTRLEN];
-//             const char* r = inet_ntop(addr->sin_family, &addr->sin_addr, buf, sizeof(buf));
-//             if (!r) {
-//                 fprintf(stderr, "inet_ntop failed v4\n");
-//             }
-
-//             printf("Addresss: %s, port: %d\n\n", buf, ntohs(addr->sin_port));
-//             break;
-//         }
-//         case AF_INET6: {
-//             struct sockaddr_in6* addr = (struct sockaddr_in6*)ptr->ai_addr;
-//             char buf[INET6_ADDRSTRLEN];
-//             const char* r = inet_ntop(addr->sin6_family, &addr->sin6_addr, buf, sizeof(buf));
-//             if (!r) {
-//                 fprintf(stderr, "inet ntp failed v6\n");
-//             }
-//             printf("Ipv6 addr: %s\n", buf);
-//             break;
-//         }
-//         default: {
-//             fprintf(stderr, "not supported\n");
-//             break;
-//         }
-//     }
-// }
-
-// freeaddrinfo(result);
